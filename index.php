40706e296d21282332e7052f594fa2a3d36084f6
AngeloGerardTerrible <ant4759@students.uc-bcf.edu.ph>
2024-10-09 21:03:13 +0800

    Finished 2nd requirement for activity 1

--- index.php
+++ index.php
@@ -7,11 +7,23 @@
 </head>
 <body>
     <?php
+    echo "Activity 1<br>1.1) ";
     $counter = 1;
     while($counter <= 10){
         echo $counter." ";
         $counter++;
     }
+    echo "<br>1.2) ";
     ?>
+    
+    <?php
+    $i = 2;
+    while ($i <= 20) {
+        echo $i . " ";
+        $i += 2;
+    }
+    ?>
+
+
 </body>
 </html>
